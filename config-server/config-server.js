8d27d150635aea962fc3c7f607502109eebb6ef0
Your Name <you@example.com>
2023-05-29 00:26:32 +0700

    a

--- config-server/config-server.js
+++ config-server/config-server.js
@@ -1,2 +1,2 @@
-const server = "https://474f-2402-800-61c6-8a4a-50d5-9a68-c57f-69c.ngrok-free.app";
+const server = "https://7845-2402-800-61c6-8a4a-38e1-ef24-4edc-be95.ngrok-free.app";
 //const server = "http://localhost";
